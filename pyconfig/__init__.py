49232a84b5dc3bcc20f68f06ffac56e4b84e7187
Chuong Ngo <cngo.github@gmail.com>
2017-11-10 17:22:15 -0500

    Added pyconfig.

---
+++ pyconfig/__init__.py
@@ -0,0 +1,11 @@
+"""
+    User friendly configuration package.
+"""
+
+from . import pyconfig
+
+from .pyconfig import *
+
+default = pyconfig.PyConfig()
+
+__version__ = '0.1'
---
+++ pyconfig/pyconfig.py
@@ -0,0 +1,22 @@
+"""
+    The configuration object.
+"""
+
+class PyConfig(dict):
+    """
+        Dict-wrapped object used for configurations.
+    """
+    def __getattr__(self, name):
+        if name in self:
+            return self[name]
+        else:
+            raise AttributeError('No such attribute: ' + name)
+
+    def __setattr__(self, name, value):
+        self[name] = value
+
+    def __delattr__(self, name):
+        if name in self:
+            del self[name]
+        else:
+            raise AttributeError('No such attribute: ' + name)
